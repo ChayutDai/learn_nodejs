7ecb04e8a77e96ff3f88fcec467a607dd1b1a825
DESKTOP-R06I7FV\DAINAMIC <chayut.dai@gmail.com>
2018-02-20 11:51:28 +0700

    Requiring Your Own Files / module.export

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,18 +2,30 @@ console.log('starting app');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./note.js');
 
-var user = os.userInfo();
+var res = notes.addNote();
+console.log(res);
+
+console.log('Result:',notes.add(2,-5));
 
 
-fs.appendFile('greetings.txt',`Hello ${user.username}!`)
+
+
+
+
+
+// ----------- using Require ----------
 /*
-    fs.appendFile('greetings.txt','Hello '+ user.username +'!',function (err){
+    var user = os.userInfo();
+    fs.appendFile('greetings.txt',`Hello ${user.username}! You are ${notes.age}`,function (err){
         if (err){
             console.log('Unable to write to file');
         }
     });
-*/
-/*
+
+     fs.appendFile('greetings.txt',`Hello ${user.username}!`)
+
     fs.appendFileSync('greetings.txt','Hello world !');
 */
+// ----------- using Require ----------
---
+++ notes-node/note.js
@@ -0,0 +1,13 @@
+console.log('start note.js');
+//console.log(module);
+
+//module.exports.age = 25;
+
+module.exports.addNote = () => {
+    console.log('addNote');
+    return 'New note';
+};
+
+module.exports.add = (a,b) => {
+    return a+b;
+};
