b684dcc1070f2006743199c2dc3cd4caf1bda788
DESKTOP-R06I7FV\DAINAMIC <chayut.dai@gmail.com>
2018-02-20 12:22:07 +0700

    Using 3rd party modules / npm install lodash

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,17 +2,24 @@ console.log('starting app');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./note.js');
 
-var res = notes.addNote();
-console.log(res);
+//-------- using 3rd party module ----------
 
-console.log('Result:',notes.add(2,-5));
+console.log(_.isString(true));
+console.log(_.isString('True'));
+console.log(_.uniq([2,1,2,'chayut','chayut']));
 
+//-------- using 3rd party module ----------
 
+//--------- requiring your own files  --------
 
+//var res = notes.addNote();
+//console.log(res);
+//console.log('Result:',notes.add(2,-5));
 
-
+//--------- requiring your own files  --------
 
 
 // ----------- using Require ----------
