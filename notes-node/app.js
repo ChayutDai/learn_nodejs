8a63e5b670a76a25dba26d00a180cba2855b21ae
DESKTOP-R06I7FV\DAINAMIC <chayut.dai@gmail.com>
2018-02-22 12:33:39 +0700

    Topic : Removing Note

--- notes-node/app.js
+++ notes-node/app.js
@@ -28,7 +28,10 @@ if(command === 'add'){
 }else if(command === 'read'){
     notes.getNote(argv.title)
 }else if (command === 'remove'){
-    notes.removeNote(argv.title)
+    var noteRemoved= notes.removeNote(argv.title);
+    var message = noteRemoved ? 'Note was removed' : 'Note not found';
+    console.log(noteRemoved);
+    console.log(message);
 }
 else{
     console.log('Comman not recognized')
--- notes-node/note.js
+++ notes-node/note.js
@@ -39,8 +39,15 @@ var getAll = () => {
 var getNote = (title) => {
     console.log('Getting notes',title);
 };
+
 var removeNote = (title) => {
-    console.log('Removing note', title);
+    //fetch notes
+    var notes = fetchNotes();
+    //fillter notes, remove the one with title of argument
+    var filteredNotes = notes.filter((note) => note.title !== title);
+    //save
+    saveNotes(filteredNotes);
+    return notes.length !== filteredNotes.length;
 };
 
 module.exports ={
