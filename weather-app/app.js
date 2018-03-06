034c33f30ad42941c6fb98d477ff706e79eb0fdb
DESKTOP-R06I7FV\DAINAMIC <chayut.dai@gmail.com>
2018-03-06 20:07:58 +0700

    Topic : Wiring Up Weather Search

--- weather-app/app.js
+++ weather-app/app.js
@@ -24,3 +24,19 @@ geocode.geocodeAddress(argv.address,(errorMessage,results)=>{
     }
 });
 //console.log(argv);
+//be63d0ebd6ceb08d0695a1e56a328d62
+
+const request2 = require('request');
+request2({
+ url: 'https://api.darksky.net/forecast/be63d0ebd6ceb08d0695a1e56a328d62/37.8267,-122.4233',
+ json: true 
+
+},(error,response,results) => {
+    if(!error && response.statusCode === 200){  
+        console.log(results.currently.temperature);
+      }else if (response.statusCode === 400 ){
+        console.log('Unable to fetch weather.'); 
+      }else{
+        console.log(error); 
+      }
+});
